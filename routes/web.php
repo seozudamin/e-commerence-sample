3c56089413cbf4ec396522ea3c830691375d19d7
seozudamin <karlarnoldcorrea24@gmail.com>
2025-04-04 09:54:58 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\HomeController;
+Route::get("/", [HomeController::class,"index"]);
